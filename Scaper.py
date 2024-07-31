c0787f4f6e0fb4c79307e60515a80cb9730856ca
Suleiman <Suleimanayomide716@gmail.com>
2024-07-31 11:36:59 +0100

    Scaper.py

--- Scaper.py
+++ Scaper.py
@@ -5,6 +5,6 @@ response = request.get (url)
 soup = 
 Beautifulsoup (response.content, 'html parser')
 data =
-soup.get_text(seperator = '\n'
+soup.get_text(seperator = '\n',
 strip = True)
 return data
